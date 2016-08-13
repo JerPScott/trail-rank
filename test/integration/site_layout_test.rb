dab54165bb8663730ab30e1b87265bb8c495bd9e
Jeremy Scott <Jer.P.Scott@gmail.com>
2016-08-13 22:04:02 +0000

    Added users controller as the start of the users system

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -12,5 +12,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title
     get contact_path
     assert_select "title", full_title("Contact")
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 end
